752ced2e044063f0e815a65cb53a252f8bc135f3
Roksana <roksananiechcial@gmail.com>
2017-04-10 16:30:12 +0200

    Project initialized. Added maze creation and player setups

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public Maze mazePrefab;
+
+    private Maze mazeInstance;
+
+    public Player playerPrefab;
+
+    private Player playerInstance;
+
+    // Use this for initialization
+    public void Start () {
+        StartCoroutine(BeginGame());
+        
+	}
+	
+	// Update is called once per frame
+	public void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            RestartGame();
+        }
+
+    }
+
+    private IEnumerator BeginGame()
+    {
+        //Camera.main.rect = new Rect(0f, 0f, 1f, 1f);
+        mazeInstance = Instantiate(mazePrefab) as Maze;
+        yield return StartCoroutine(mazeInstance.Generate());
+        playerInstance = Instantiate(playerPrefab) as Player;
+        playerInstance.SetLocation(mazeInstance.GetCell(mazeInstance.RandomCoordinates));
+        //Camera.main.rect = new Rect(0f, 0f, 0.5f, 0.5f);
+    }
+
+    public void RestartGame() {
+        StopAllCoroutines();
+        Destroy(mazeInstance.gameObject);
+        if (playerInstance != null)
+        {
+            Destroy(playerInstance.gameObject);
+        }
+        StartCoroutine(BeginGame());
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private MazeCell currentCell;
+    private MazeDirection currentDirection;
+
+    public void SetLocation(MazeCell cell) {
+        currentCell = cell;
+        transform.localPosition = cell.transform.localPosition;
+    }
+
+    private void Move(MazeDirection direction) {
+        MazeCellEdge edge = currentCell.GetEdge(direction);
+        if (edge is MazePassage)
+        {
+            SetLocation(edge.otherCell);
+        }
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Move(currentDirection);
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Move(currentDirection.GetNextClockwise());
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Move(currentDirection.GetOpposite());
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Move(currentDirection.GetNextCounterclockwise());
+        }
+        else if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Rotate(currentDirection.GetNextCounterclockwise());
+        }
+        else if (Input.GetKeyDown(KeyCode.E))
+        {
+            Rotate(currentDirection.GetNextClockwise());
+        }
+    }
+
+    private void Rotate(MazeDirection direction)
+    {
+        transform.localRotation = direction.ToRotation();
+        currentDirection = direction;
+    }
+}
