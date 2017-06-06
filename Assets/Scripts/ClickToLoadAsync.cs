6144eea38caad9904a45b8bb680c6d75041da5d0
Roksana <roksananiechcial@gmail.com>
2017-06-06 16:07:58 +0200

    Added main menu

---
+++ Assets/Scripts/ClickToLoadAsync.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ClickToLoadAsync : MonoBehaviour
+{
+
+    public Slider loadingBar;
+    public GameObject loadingImage;
+
+
+    private AsyncOperation async;
+
+
+    public void ClickAsync(int level)
+    {
+        loadingImage.SetActive(true);
+        StartCoroutine(LoadLevelWithBar(level));
+    }
+
+
+    IEnumerator LoadLevelWithBar(int level)
+    {
+        async = SceneManager.LoadSceneAsync(level); ;
+        while (!async.isDone)
+        {
+            loadingBar.value = async.progress;
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/LoadOnClick.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadOnClick : MonoBehaviour {
+    public GameObject loadingImage;
+
+    public void LoadScene(string scene)
+    {
+        loadingImage.SetActive(true);
+        SceneManager.LoadScene(scene);
+    }
+}
